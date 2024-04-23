66855d782fe27085d22ba3ed55aa4be7f6d394a4
Michael <michael8t6@gmail.com>
2024-04-23 13:57:32 +0100

    patching partial refactor complete.

--- src/components/tableButtons/PartialConfirm.tsx
+++ src/components/tableButtons/PartialConfirm.tsx
@@ -1,13 +1,16 @@
 import { useRef } from "react";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import { setPart } from "../../slices/purchaseOrders";
+import axios from "../../utils/interceptors";
+import { AxiosResponse } from "axios";
+import { setToast } from "../../slices/alert";
 
 type Props = {
   name: string;
 };
 
 const PartialConfirm: React.FC<Props> = ({ name }) => {
-  const { partNumbers } = useAppSelector((state) => state.purchase.order!);
+  const { partNumbers, purchaseOrder } = useAppSelector((state) => state.purchase.order!);
   const ref = useRef<HTMLInputElement | null>(null);
   const dispatch = useAppDispatch();
 
@@ -18,7 +21,21 @@ const PartialConfirm: React.FC<Props> = ({ name }) => {
 
     if (!ref.current || !ref.current.checked) return;
 
-    //Call api
+    type StatusRes = { status: number; token: string };
+    const res: AxiosResponse<StatusRes> = await axios.patch(
+      `/purchase/set-partial/${purchaseOrder}/${name}`
+    );
+
+    if (res.status !== 200 || !res.data.status) {
+      return dispatch(
+        setToast({
+          type: "error",
+          message:
+            "oops something went wrong there, contact michael with the purchase order number",
+          show: true,
+        })
+      );
+    }
 
     const copy = structuredClone(part);
     copy.partial = 1;
