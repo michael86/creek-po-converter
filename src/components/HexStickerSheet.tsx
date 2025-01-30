70ef44c826188193f2fcbae69f402ba5b8d99b25
Michael <michael8t6@gmail.com>
2025-01-30 11:00:09 +0000

    Hex stickers complete

---
+++ src/components/HexStickerSheet.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+
+// Type definition for the data
+
+interface StickerSheetProps {
+  data: {
+    hex: string;
+    decimal: number;
+  }[];
+  topMargin: number;
+  sideMargin: number;
+  verticalPitch: number;
+  horizontalPitch: number;
+  labelHeight: number;
+  labelWidth: number;
+  numberAcross: number;
+  numberDown: number;
+}
+
+const HexStickerSheet: React.FC<StickerSheetProps> = ({
+  data,
+  topMargin,
+  sideMargin,
+  verticalPitch,
+  horizontalPitch,
+  labelHeight,
+  labelWidth,
+  numberAcross,
+  numberDown,
+}) => {
+  // Calculate the total height and width of the grid, factoring in the labels and spacing
+  const totalHeight = labelHeight * numberDown + verticalPitch * (numberDown - 1);
+  const totalWidth = labelWidth * numberAcross + horizontalPitch * (numberAcross - 1);
+
+  const generateStyles = (): React.CSSProperties => {
+    return {
+      marginTop: `${topMargin}cm`,
+      marginLeft: `${sideMargin}cm`,
+      display: "grid",
+      gridTemplateColumns: `repeat(${numberAcross}, ${labelWidth}cm)`,
+      gridTemplateRows: `repeat(${numberDown}, ${labelHeight}cm)`,
+      gap: `${verticalPitch}cm ${horizontalPitch}cm`,
+      position: "relative" as "relative",
+      height: `${totalHeight}cm`, // Updated height calculation
+      width: `${totalWidth}cm`, // Updated width calculation
+    };
+  };
+
+  useEffect(() => {
+    window.print();
+  }, []);
+
+  return (
+    <div className="sticker-sheet" style={generateStyles()}>
+      {data.map((item, index) => {
+        // const hexOrDec = index % 2 === 0 ? item.hex : item.decimal.toString();
+        return (
+          <div
+            key={index}
+            style={{
+              width: `${labelWidth}cm`,
+              height: `${labelHeight}cm`,
+              border: "1px solid black",
+              textAlign: "center",
+              lineHeight: `${labelHeight}cm`,
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {item.hex}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default HexStickerSheet;
--- src/pages/CreateHexSticker.tsx
+++ src/pages/CreateHexSticker.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { Template, BLANK_PDF } from "@pdfme/common";
-import { generate } from "@pdfme/generator";
 
 import {
   Input,
@@ -14,77 +12,16 @@ import {
 
 import "../styles/create_hex.css";
 import HexTable from "../components/HexTable";
+import HexStickerSheet from "../components/HexStickerSheet";
 
 const convertToHex = (number: number) => number.toString(16);
 const convertToDec = (number: string) => parseInt(number, 16);
-
-const testPDF = () => {
-  const fontSize: number = 6;
-  const labelWidth: number = 16;
-  const labelHeight: number = 4;
-  const sheetHeight: number = 297;
-  const sheetWidth: number = 210;
-  const sheetCols: number = 10;
-  const sheetRows: number = 36;
-
-  const template: Template = {
-    basePdf: BLANK_PDF,
-
-    /**
-     * Schema is going to be our label positions assumed 
-     * the name is going to be used for the inputs to know where to put our label.
-     * Need to addjust the margin top and so on...
-     * 
-     * Template details: 
-     * top margin: 6
-     * sid margin: 8
-     * vertical pitch: 8 - Pitch is from the top side of the sticker to the top of the next, includes the sticker and gap
-     * horizontal pitch: 20 - same as above but from the left side of the sticker to the left of the next
-     * page size: 21 x 297 - A4 
-     * label heigh: 4
-     * label width: 16
-     * number across: 10 - columns
-     * number down 36 - rows
-    
-    */
-    schemas: [
-      [
-        {
-          name: "0",
-          type: "text",
-          position: { x: (sheetHeight / sheetRows) * 0, y: (sheetWidth / sheetCols) * 0 },
-          width: labelWidth,
-          height: labelHeight,
-          alignment: "center",
-          fontSize: fontSize,
-        },
-        {
-          name: "13",
-          type: "text",
-          position: { x: (sheetHeight / sheetRows) * 6, y: (sheetWidth / sheetCols) * 1 },
-          width: labelWidth,
-          height: labelHeight,
-          alignment: "center",
-          fontSize: fontSize,
-        },
-      ],
-    ],
-  };
-
-  const inputs = [{ 0: "ermmmmm", 13: "yeaahhhhhh" }];
-
-  generate({ template, inputs }).then((pdf) => {
-    console.log("ya yeet ", pdf);
-
-    const blob = new Blob([pdf.buffer], { type: "application/pdf" });
-    window.open(URL.createObjectURL(blob));
-  });
-};
 
 const CreateHexSticker = () => {
   const [value, setValue] = useState("");
   const [count, setCount] = useState(1);
   const [radio, setRadio] = useState(0);
+  const [print, setPrint] = useState(false);
   const [data, setData] = useState<{ hex: string; decimal: number }[]>([]);
 
   const manageConversion = () => {
@@ -108,63 +45,83 @@ const CreateHexSticker = () => {
     }
 
     setData(newData);
-
-    testPDF();
   };
 
   const setRadioValue = (value: string) => setRadio(value.toLowerCase() === "decimal" ? 0 : 1);
 
   return (
     <>
-      <h1 className="hex-title">Create Hex</h1>
+      {print ? (
+        <HexStickerSheet
+          data={data}
+          topMargin={1.2}
+          sideMargin={0.4}
+          verticalPitch={0}
+          horizontalPitch={2.0 - 1.7}
+          labelHeight={1.0}
+          labelWidth={1.7}
+          numberAcross={10}
+          numberDown={27}
+        />
+      ) : (
+        <>
+          <h1 className="hex-title">Create Hex</h1>
 
-      <form className="hex-form">
-        <InputLabel>
-          Start Count:
-          <Input
-            color="primary"
-            margin="dense"
-            name="start-count"
-            required
-            type="text"
-            value={value}
-            onChange={(e) => setValue(e.target.value.toUpperCase())}
-          />
-        </InputLabel>
+          <form className="hex-form">
+            <InputLabel>
+              Start Count:
+              <Input
+                color="primary"
+                margin="dense"
+                name="start-count"
+                required
+                type="text"
+                value={value}
+                onChange={(e) => setValue(e.target.value.toUpperCase())}
+              />
+            </InputLabel>
 
-        <InputLabel>
-          Amount Required:
-          <Input
-            color="primary"
-            name="count-amount"
-            required
-            type="number"
-            onChange={(e) => setCount(+e.target.value)}
-          />
-        </InputLabel>
+            <InputLabel>
+              Amount Required:
+              <Input
+                color="primary"
+                name="count-amount"
+                required
+                type="number"
+                onChange={(e) => setCount(+e.target.value)}
+              />
+            </InputLabel>
 
-        <FormControl>
-          <RadioGroup
-            defaultValue="decimal"
-            name="radio-buttons-group"
-            onChange={(e) => setRadioValue(e.target.value)}
-          >
-            <FormControlLabel value="decimal" control={<Radio />} label="decimal" />
-            <FormControlLabel value="hex" control={<Radio />} label="hex" />
-          </RadioGroup>
-        </FormControl>
+            <FormControl>
+              <RadioGroup
+                defaultValue="decimal"
+                name="radio-buttons-group"
+                onChange={(e) => setRadioValue(e.target.value)}
+              >
+                <FormControlLabel value="decimal" control={<Radio />} label="decimal" />
+                <FormControlLabel value="hex" control={<Radio />} label="hex" />
+              </RadioGroup>
+            </FormControl>
 
-        <div className="button-container">
-          <Button variant="contained" onClick={() => manageConversion()}>
-            Generate
-          </Button>
-        </div>
-      </form>
+            <div className="button-container">
+              <Button variant="contained" onClick={() => manageConversion()}>
+                Generate
+              </Button>
 
-      {data.length > 0 && (
-        <section className="data-table">
-          <HexTable data={data} />
-        </section>
+              {data.length > 0 && (
+                <Button variant="contained" onClick={() => setPrint(true)}>
+                  Print
+                </Button>
+              )}
+            </div>
+          </form>
+
+          {data.length > 0 && (
+            <section className="data-table">
+              <HexTable data={data} />
+            </section>
+          )}
+        </>
       )}
     </>
   );
