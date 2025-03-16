66bc1b2a1a02badd5b091e0e712512cf8a7718b8
michael86 <michael8t6@gmail.com>
2025-03-16 11:26:22 +0000

    auth route input schema

--- src/schemas/authRoutes.ts
+++ src/schemas/authRoutes.ts
@@ -1,2 +1,29 @@
-export const logingFields = ["email", "password"];
-export const registerFields = [...logingFields, "password"];
+export const logingFields = [
+  {
+    name: "email",
+    label: "Email",
+    type: "text",
+    variant: "standard",
+  },
+  {
+    name: "password",
+    label: "Password",
+    type: "password",
+    variant: "standard",
+  },
+];
+export const registerFields = [
+  ...logingFields,
+  {
+    name: "confirm-password",
+    label: "Confirm Password",
+    type: "password",
+    variant: "standard",
+  },
+  {
+    name: "name",
+    label: "name",
+    type: "text",
+    variant: "standard",
+  },
+];
