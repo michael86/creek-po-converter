45dc7d5e276e2d212d4407d894c46ab6619c8487
Michael <michael8t6@gmail.com>
2025-06-12 16:32:07 +0100

    Fixed table history not showing due to new db schema

--- src/components/purchaseOrderTable/TableBody.tsx
+++ src/components/purchaseOrderTable/TableBody.tsx
@@ -12,6 +12,7 @@ type Props = {
 };
 
 const TableBody: React.FC<Props> = ({ items, editMode, setShowModal, setModalRow, refetch }) => {
+  console.log("items ", items);
   return (
     <Tbody>
       {items.map((row, index) => (
