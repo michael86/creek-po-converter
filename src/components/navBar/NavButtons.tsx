e5dbd99a7bfa1bdf8bbcf02105cf706ab729a8d2
Michael <michael8t6@gmail.com>
2025-06-16 13:51:07 +0100

    Moved gen purchase order action into util

--- src/components/navBar/NavButtons.tsx
+++ src/components/navBar/NavButtons.tsx
@@ -9,6 +9,7 @@ import SnackBar from "../SnackBar";
 import { useState } from "react";
 import jsPDF from "jspdf";
 import { PurchaseOrderLabel } from "../../types/labels";
+import { genPurchaseOrderLabels } from "../../utils/Nav/purchaseOrders";
 
 export default function NavButtons() {
   const [showSnack, setShowSnack] = useState<boolean>(false);
@@ -47,56 +48,7 @@ export default function NavButtons() {
       {
         role: 3,
         label: "print",
-        action: () => {
-          if (Object.keys(purchaseOrder.labels).length > 0) {
-            const doc = new jsPDF({
-              unit: "mm",
-              format: [62, 40], // label size in mm
-              orientation: "landscape",
-            });
-
-            // Flatten labels into a single array of PurchaseOrderLabel
-            const allLabels: PurchaseOrderLabel[] = Object.values(purchaseOrder.labels).flatMap(
-              (historyMap) => Object.values(historyMap)
-            );
-
-            allLabels.forEach((value, index) => {
-              if (index > 0) doc.addPage(); // new 62×40 mm page
-              const pageWidth = doc.internal.pageSize.getWidth();
-
-              const margin = 2; // 2 mm left/right margin
-              const maxWidth = 62 - margin * 2;
-              let y = 6;
-
-              doc.setFontSize(12);
-              doc.text(value.partNumber, pageWidth / 2, y, { align: "center" });
-              doc.text(value.purchaseOrder, pageWidth / 2, (y += 5), { align: "center" });
-              doc.text(`Qty: ${value.quantityReceived}`, pageWidth / 2, (y += 5), {
-                align: "center",
-              });
-              y += 5;
-
-              const desc = `${value.description}`;
-              const wrapped = doc.splitTextToSize(desc, maxWidth);
-              doc.text(wrapped, pageWidth / 2, y, { align: "center" });
-              y += wrapped.length * 2.5;
-
-              doc.text(new Date(value.dateReceived).toLocaleDateString(), pageWidth / 2, (y += 5), {
-                align: "center",
-              });
-              doc.text(value.storageLocation || "Location", pageWidth / 2, (y += 5), {
-                align: "center",
-              });
-            });
-
-            const blob = doc.output("blob");
-            const url = URL.createObjectURL(blob);
-            window.open(url, "_blank");
-          } else {
-            setShowSnack(true);
-          }
-        },
-
+        action: () => genPurchaseOrderLabels(purchaseOrder.labels, setShowSnack),
         sx: { mr: 2.5 },
       },
     ],
