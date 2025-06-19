a5226f76f1de4e62b7f60b190dd2ea9dece3c6d0
Michael <michael8t6@gmail.com>
2025-06-19 16:46:51 +0100

    type comment for clarity

--- src/types/labels.ts
+++ src/types/labels.ts
@@ -7,5 +7,17 @@ export interface PurchaseOrderLabel {
   storageLocation: string | null;
 }
 
-// 2) A map from historyId → that single label
+/**
+ * As there's multiple partnumbers, we need to group the labels by part numbers to prevent them being overwritten
+ * if there's multiple rows for the same part (scheduled deliveries across multipel months), we can just group then
+ * under the same part number as it's on;y the date that will change
+ * {
+ *  partNumber: {
+ *    PurchaseOrderLabel
+ *  },
+ *  partNumber2: {
+ *    PurchaseOrderLabel
+ *  },
+ * }
+ */
 export type PurchaseOrderLabelsMap = Record<string, Record<number, PurchaseOrderLabel>>;
