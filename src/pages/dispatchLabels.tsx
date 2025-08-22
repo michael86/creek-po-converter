cb6d4ecd7a2678f3e57b1be4a72786158f0801c4
Michael <michael8t6@gmail.com>
2025-08-22 09:31:59 +0100

    updated dispatch label template

--- src/pages/dispatchLabels.tsx
+++ src/pages/dispatchLabels.tsx
@@ -119,41 +119,51 @@ const StickerTemplate = () => {
     }
   }
 
-  .sheet {
-    width: 210mm;
-    height: 297mm;
-    display: grid;
-    grid-template-columns: repeat(3, 63.5mm);
-    grid-auto-rows: 38.1mm;
-    gap: 0 3.5mm;
-    padding: 14.5mm 6.5mm;
-    box-sizing: border-box;
-  }
+.sheet {
+  width: 210mm;
+  height: 297mm;
+  display: grid;
+  grid-template-columns: repeat(3, 70mm);
+  grid-template-rows: repeat(5, 51mm);
+  column-gap: 0;
+  row-gap: 0;
+  padding: 21.40mm 0; /* top & bottom margin only */
+  box-sizing: border-box;
+}
 
-  .label {
-    box-sizing: border-box;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    position: relative;
-    text-align: left;
-  }
+.label {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  text-align: left;
+  overflow: hidden; /* prevent anything spilling */
+}
 
-  .sticker-logo {
-    padding-left: 40mm !important;
-    position: absolute;
-    top: 2mm;
-    right: 2mm;
-    width: 100% !important;
-  }
+.sticker-logo {
+  position: absolute;
+  top: 2.5mm !important;
+  right: 5.5mm !important;       /* push away from right edge */
+  width: 22mm !important;      /* slightly smaller so it fits */
+  height: auto;
+}
 
-  .content-container {
-    position: absolute;
-    top: 3.5mm;
-    left: 2mm;
-    line-height: 1.4;
-    font-size: 15pt;
-  }
+.content-container {
+  position: absolute;
+  top: 15mm !important;        /* push text further down */
+  left:4mm !important;        /* add more left padding */
+  line-height: 1.4;
+  font-size: 13pt;  /* make font larger */
+  width: 90%
+}
+.content-container div {
+  margin-bottom: 2mm !important; /* space out lines */
+  border-bottom: 1pt solid black; /* underline full width */
+  width: 100%;                   /* stretch underline across */
+  // padding-bottom: 1mm;            /* breathing room above line */
+  box-sizing: border-box;
+}
 `}</style>
     </>
   );
